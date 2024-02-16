e3b8c6a4a4564773941f5c56451070b086d8843d
shakiaripa <shakiaripa@gmail.com>
2024-02-16 23:56:54 +0600

    added home screen ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
